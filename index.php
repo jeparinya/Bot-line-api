ea4f7a4aa94cc5a2881670ee6d62b6fa1bd86c35
jeparinya <55320870+jeparinya@users.noreply.github.com>
2020-11-02 08:55:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,43 +1,44 @@
 <?php
-// URL API LINE
-$API_URL = 'https://api.line.me/v2/bot/message';// ใส่ Channel access token (long-lived)
-$ACCESS_TOKEN = 'epNXXLLGhKkTZyfsYcZI+KuWaV24f3lbCMFftmL0Kvj1Wooz754oPR7w4eIBeWTTY0s4QaEjiuMZH1DefFTA/TpWbVnuGVTLovjXp8Rt1vYeotAOVuPL13q3R1aE3MbMK38qOdvvb7+jTK8decddPgdB04t89/1O/w1cDnyilFU=';// ใส่ Channel Secret
-$CHANNEL_SECRET = 'bc251bec6b2a4f7c0c71d3b8121f6d7f';
-
-// Set HEADER
-$POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);// Get request content
-$request = file_get_contents('php://input');// Decode JSON to Array
-$request_array = json_decode($request, true);
-
-function send_reply_message($url, $post_header, $post_body)
-{
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    $result = curl_exec($ch);
-    curl_close($ch);
-
-    return $result;
-}
-
-
-
-if ( sizeof($request_array['events']) > 0 ) {
-    foreach ($request_array['events'] as $event) {
-    
-    $reply_message = '';
-    $reply_token = $event['replyToken'];
-    $data = [
-       'replyToken' => $reply_token,
-       'messages' => [
-          ['type' => 'text', 
-           'text' => json_encode($request_array)]
-       ]
-    ];
-    $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
-    $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);      echo "Result: ".$send_result."\r\n";   }
-}
-echo "OK";
+   $accessToken = "epNXXLLGhKkTZyfsYcZI+KuWaV24f3lbCMFftmL0Kvj1Wooz754oPR7w4eIBeWTTY0s4QaEjiuMZH1DefFTA/TpWbVnuGVTLovjXp8Rt1vYeotAOVuPL13q3R1aE3MbMK38qOdvvb7+jTK8decddPgdB04t89/1O/w1cDnyilFU=";//copy ข้อความ Channel access token ตอนที่ตั้งค่า
+$content = file_get_contents('php://input');
+   $arrayJson = json_decode($content, true);
+$arrayHeader = array();
+   $arrayHeader[] = "Content-Type: application/json";
+   $arrayHeader[] = "Authorization: Bearer {$accessToken}";
+//รับข้อความจากผู้ใช้
+   $message = $arrayJson['events'][0]['message']['text'];
+//รับ id ว่ามาจากไหน
+   if(isset($arrayJson['events'][0]['source']['userId']){
+      $id = $arrayJson['events'][0]['source']['userId'];
+   }
+   else if(isset($arrayJson['events'][0]['source']['groupId'])){
+      $id = $arrayJson['events'][0]['source']['groupId'];
+   }
+   else if(isset($arrayJson['events'][0]['source']['room'])){
+      $id = $arrayJson['events'][0]['source']['room'];
+   }
+#ตัวอย่าง Message Type "Text + Sticker"
+   if($message == "สวัสดี"){
+      $arrayPostData['to'] = $id;
+      $arrayPostData['messages'][0]['type'] = "text";
+      $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา".$id;
+      $arrayPostData['messages'][1]['type'] = "sticker";
+      $arrayPostData['messages'][1]['packageId'] = "2";
+      $arrayPostData['messages'][1]['stickerId'] = "34";
+      pushMsg($arrayHeader,$arrayPostData);
+   }
+function pushMsg($arrayHeader,$arrayPostData){
+      $strUrl = "https://api.line.me/v2/bot/message/push";
+$ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL,$strUrl);
+      curl_setopt($ch, CURLOPT_HEADER, false);
+      curl_setopt($ch, CURLOPT_POST, true);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+      $result = curl_exec($ch);
+      curl_close ($ch);
+   }
+exit;
+?>
